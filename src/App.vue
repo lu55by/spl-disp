a395e624e213ca1c45db9b8041a7c39cbe8fab69
lu55by <lussbyandros@gmail.com>
2025-12-14 17:29:28 +0800

    Reorganized the component tree in App.vue to toggle the components in a more convenient way.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import MainUI from "./components/ui/MainUI.vue";
 import LoadingOverlay from "./components/ui/LoadingOverlay.vue";
-import SplicingModels from "./components/three/SplicingModels.vue";
-
-// import ThreeScene from "./components/three/ThreeScene.vue";
+// import ThreeSceneTst from "./components/three/ThreeSceneTst.vue";
+import SplicingModelsMainUIContainer from "./components/SplicingModelsMainUIContainer.vue";
 </script>
 
 <template>
@@ -11,9 +9,8 @@ import SplicingModels from "./components/three/SplicingModels.vue";
   <div
     class="w-full h-svh bg-[linear-gradient(90deg,rgb(27,27,37),rgb(54,54,72),rgb(27,27,37))]"
   >
-    <MainUI />
     <LoadingOverlay />
-    <SplicingModels />
-    <!-- <ThreeScene /> -->
+    <!-- <ThreeSceneTst /> -->
+    <SplicingModelsMainUIContainer />
   </div>
 </template>
---
+++ src/components/SplicingModelsMainUIContainer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SplicingModels from "./three/SplicingModels.vue";
+import MainUI from "./ui/MainUI.vue";
+</script>
+
+<template>
+  <MainUI />
+  <SplicingModels />
+</template>
