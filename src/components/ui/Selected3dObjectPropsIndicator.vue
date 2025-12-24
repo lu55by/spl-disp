05ff4e902500d7803f47de07bad2259d0c87046a
lu55by <lussbyandros@gmail.com>
2025-12-24 18:11:13 +0800

    Fixed some linting errors.

--- src/components/ui/Selected3dObjectPropsIndicator.vue
+++ src/components/ui/Selected3dObjectPropsIndicator.vue
@@ -294,11 +294,13 @@ const handleInput = () => {
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
   -webkit-appearance: none;
+  appearance: none;
   margin: 0;
 }
 
 /* Firefox */
 input[type="number"] {
   -moz-appearance: textfield;
+  appearance: textfield;
 }
 </style>
