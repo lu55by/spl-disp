0a058ddb028e3bec7df073776be04069f6a66d92
lu55by <lussbyandros@gmail.com>
2025-12-27 15:07:23 +0800

    A little changes.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import {createPinia} from "pinia";
-import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
-import './style.css'
-import 'vue3-toastify/dist/index.css';
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+import "./style.css";
+import "vue3-toastify/dist/index.css";
+import App from "./App.vue";
 
 const app = createApp(App);
 app.use(createPinia());
@@ -11,4 +11,4 @@ app.use(Vue3Toastify, {
   autoClose: 3000,
   position: "top-center",
 } as ToastContainerOptions);
-app.mount('#app');
+app.mount("#app");
