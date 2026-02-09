33ce1b460666d54df960f0c4e20db3b4b67f3c11
lu55by <lussbyandros@gmail.com>
2026-02-09 16:41:55 +0800

    Change history implementation used by the router from `createWebHistory` to `createWebHashHistory`.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import SplicingModelsMainContainer from "../components/SplicingModelsMainContainer.vue";
 import PageNotFound from "../components/ui/PageNotFound.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
